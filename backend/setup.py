a9242708f069035721bcf9300bcf7590a19b58e1
Andreas Blomqvist <andreas@onecoder.com>
2025-08-11 09:00:53 +0200

    Bump version: frontend 1.6.11  1.6.12, backend 0.5.12 → 0.5.13

--- backend/setup.py
+++ backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple-simulation-backend",
-    version="0.5.12",
+    version="0.5.13",
     packages=find_packages(),
     install_requires=[
         "fastapi>=0.68.0",
